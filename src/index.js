3ba477e4d69824296146382c6503ddb605fcad27
Ivan Wong <ivanwong@gatech.edu>
2021-09-27 16:57:03 -0700

    created knapsack calculator component

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+
+import KnapsackCalculator from './knapsack-calculator/knapsack-calculator';
 import reportWebVitals from './reportWebVitals';
+
+import './index.css';
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <KnapsackCalculator />
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ src/knapsack-calculator/knapsack-calculator.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+import './knapsack-calculator.css';
+
+
+export default class KnapsackCalculator extends Component {
+
+    constructor(props) {
+        super(props)
+
+        this.state = {
+
+        }
+    }
+
+    render() {
+        return (
+            <div className="KnapsackCalculator">
+                Hello World!
+            </div>
+        )
+    }
+
+}
